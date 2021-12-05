ca9f6de956cf77ddb1167ce3c29c16365b884d26
pjewell <pjewell@biociphers.org>
2021-12-05 14:53:21 -0500

    -facelift on demo page ; started bootstrap

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 
 
+function initmain(){
+    storeModelData("RRDB_ESRGAN_x4", "ESRGAN_py/RRDB_ESRGAN_x4");
 
+    document.getElementById('imageUpload').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+                const output_elem = document.getElementById('result')
+                run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
 
-storeModelData("RRDB_ESRGAN_x4", "ESRGAN_py/RRDB_ESRGAN_x4");
+            }
 
-document.getElementById('imageUpload').addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-        var img = document.querySelector('img');
-        img.onload = () => {
-            URL.revokeObjectURL(img.src);  // no longer needed, free memory
-            const output_elem = document.getElementById('result')
-            run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
-
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
         }
+    });
 
-        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-    }
-});
+    document.getElementById('demo1').addEventListener('click', function() {
+        var img = document.getElementById('sm_img');
+        const output_elem = document.getElementById('result')
+        run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
+    });
 
-document.getElementById('demo1').addEventListener('click', function() {
-    var img = document.getElementById('sm_img');
-    const output_elem = document.getElementById('result')
-    run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
-});
+    document.getElementById('demo2').addEventListener('click', function() {
+        var img = document.getElementById('med_img');
+        const output_elem = document.getElementById('result')
+        run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
+    });
 
-document.getElementById('demo2').addEventListener('click', function() {
-    var img = document.getElementById('med_img');
-    const output_elem = document.getElementById('result')
-    run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
-});
+    document.getElementById('demo3').addEventListener('click', function() {
+        var img = document.getElementById('lrg_img');
+        const output_elem = document.getElementById('result')
+        run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
+    });
 
-document.getElementById('demo3').addEventListener('click', function() {
-    var img = document.getElementById('lrg_img');
-    const output_elem = document.getElementById('result')
-    run_nn(img, output_elem, modeldata["RRDB_ESRGAN_x4"]);
-});
 
+
+}
 
